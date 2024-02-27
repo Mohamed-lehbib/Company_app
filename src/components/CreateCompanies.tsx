88ecfe681f952db7fc49cc74557c07cde87aa052
Mohamed-lehbib <abeidnamohamedlehbib@gmail.com>
2024-02-27 16:08:38 +0000

    the version with the controller on the input type file

--- src/components/CreateCompanies.tsx
+++ src/components/CreateCompanies.tsx
@@ -1,12 +1,24 @@
-import { useForm } from "react-hook-form";
+import { Controller, useForm } from "react-hook-form";
 
 export default function CreateCompanies() {
   const form = useForm<Company>({ mode: "all" });
-  const { register, handleSubmit, formState, watch } = form;
+  const { register, handleSubmit, formState, control } = form;
   const { errors } = formState;
   const onSubmit = (data: Company) => {
     console.log(data);
   };
+  const handleFileChange = (file: File) => {
+    if (!isValidLogo(file)) {
+      alert("Please select a valid image file (JPEG, PNG, GIF, SVG).");
+    }
+
+    // Do something with the file if it's valid
+  };
+  const isValidLogo = (logo: File) => {
+    // Check if the file type is image or svg
+    return logo.type.startsWith("image/");
+  };
+
   return (
     <div>
       <form onSubmit={handleSubmit(onSubmit)}>
@@ -49,8 +61,25 @@ export default function CreateCompanies() {
 
         <p style={{ color: "red" }}>{errors.website?.message}</p>
 
-        {/* <label>Logo</label>
-        <input type="file" /> */}
+        <label>Logo</label>
+        <Controller
+          name="logo"
+          control={control}
+          defaultValue=""
+          render={({ field: { onChange } }) => (
+            <input
+              type="file"
+              onChange={(e) => {
+                const file = e.target.files?.[0];
+                if (file) {
+                  onChange(file);
+                  handleFileChange(file);
+                }
+              }}
+            />
+          )}
+        />
+        <br />
         <label>Type</label>
         <select {...register("type")}>
           <option value="Government">Government</option>
